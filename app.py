f69863481039a744a01821814c887e99c3e3f2e5
MZidan31 <jidan1307@gmail.com>
2025-06-23 19:36:17 +0700

    Update app message for Dev testing and push to main

--- app.py
+++ app.py
@@ -5,14 +5,10 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    # Mengambil nilai variabel lingkungan APP_ENV, jika tidak ada akan menggunakan 'Unknown'
     environment = os.getenv('APP_ENV', 'Unknown')
-    # Mengambil nama host dari container/pod (ini adalah ID replika di K8s)
     replica_id = os.getenv('HOSTNAME', 'Unknown')
 
-    # Mengembalikan pesan sambutan yang menampilkan lingkungan dan ID replika
-    return f"Halo dari aplikasi di lingkungan **{environment}**! Ini adalah replika: **{replica_id}**"
+    return f"Hello World dari aplikasi di lingkungan **{environment}**! Ini adalah replika: **{replica_id}**. Versi: 1.0 (Updated for Dev)"
 
 if __name__ == '__main__':
-    # Menjalankan aplikasi Flask di semua antarmuka (0.0.0.0) pada port 5000
     app.run(host='0.0.0.0', port=5000)
